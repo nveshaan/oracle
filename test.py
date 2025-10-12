0888177201e718a368353d8319ed62d9e240d29b
nveshaan <nveshaan@gmail.com>
2025-10-12 10:17:17 +0530

    boom

--- test.py
+++ test.py
@@ -10,46 +10,51 @@ import random
 # ------------------ Setup ------------------
 device = 'mps' if torch.backends.mps.is_available() else 'cpu'
 num_samples = 10
-# seed = 42
-# random.seed(seed)
-# np.random.seed(seed)
-# torch.manual_seed(seed)
-# torch.set_grad_enabled(False)
+seed = 1000
+random.seed(seed)
+np.random.seed(seed)
+torch.manual_seed(seed)
+torch.set_grad_enabled(False)
 
 # Load model
-model = DiT_models["DiT-L"](input_size=(5, 30)).to(device)
-ckpt_path = "checkpoints/1009_1318_model.pth"
+model = DiT_models["DiT-L"](input_size=(1, 30)).to(device)
+ckpt_path = "checkpoints/1012_0212_model.pth"
 state = torch.load(ckpt_path, map_location=device)
 if isinstance(state, dict) and 'model' in state:
     state = state['model']
 model.load_state_dict(state, strict=False)
 model.eval()
-diffusion = create_diffusion(timestep_respacing="")  # default schedule
+diffusion = create_diffusion(timestep_respacing="")
 
 # Dataset / single batch
 dataset = yf_Trendlines()
 loader = DataLoader(dataset, batch_size=1, shuffle=False)
-x, cond = list(loader)[1]  # x: (B,1,5,30)  cond: (B,1,20,30)
+x, y = list(loader)[100]
 x = x.repeat(num_samples, 1, 1, 1)
+y = y.repeat(num_samples, 1, 1, 1)
 x = x.to(device, dtype=torch.float32)
-cond = cond.repeat(num_samples, 1, 1, 1)
-cond = cond.to(device, dtype=torch.float32)
+y = y.to(device, dtype=torch.float32)
 
 # --------------- Full reverse diffusion sampling (prediction) ---------------
 with torch.no_grad():
-    # Use the diffusion sampler to generate a full sample conditioned on cond
     sample = diffusion.p_sample_loop(
         model,  # model callable
         x.shape,  # target shape
         torch.randn_like(x),  # initial noise
-        model_kwargs={"y": cond},
+        model_kwargs={"y": y},
         progress=True,
         device=device,
         clip_denoised=True,
     )
 
-gt = np.arctanh(x[0, 0, 0, :].squeeze().cpu().numpy())/0.01
-pred = np.arctanh(sample[:, 0, 0, :].squeeze().cpu().numpy())/0.01
+x = x.cpu().numpy()
+sample = sample.cpu().numpy()
+
+x_ = (np.arcsin(x[:, 1]) + np.arccos(x[:, 2]) + np.arcsin(x[:, 3]) + np.arccos(x[:, 4]) + np.arcsin(x[:, 5]) + np.arccos(x[:, 6]))/12
+sample_ = (np.arcsin(sample[:, 1]) + np.arccos(sample[:, 2]) + np.arcsin(sample[:, 3]) + np.arccos(sample[:, 4]) + np.arcsin(sample[:, 5]) + np.arccos(sample[:, 6]))/12
+
+gt = np.arctanh(x_[0, :].squeeze()/np.pi)/0.01
+pred = np.arctanh(sample_[:, :].squeeze()/np.pi)/0.01
 
 for i in range(num_samples):
     plt.plot(pred[i], label='Predicted', color='orange', alpha=0.3)
